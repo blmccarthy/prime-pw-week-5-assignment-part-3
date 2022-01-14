41479d442ee95b575fbc7f42b13dd367658c2ef8
Brant McCarthy <brant.mccarthy@me.com>
2022-01-13 19:36:43 -0600

    cleaned up / simplified code

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -3,34 +3,26 @@ console.log('***** Music Collection *****')
 let collection = [];
 
 function addToCollection( title, artist, yearPublished ) {
-  let album = {
+  let album = { // creates object with keys defined below
     title: title,
     artist: artist,
     yearPublished: yearPublished,
   }
-  collection.push(album);
-  return true;
-}
+  collection.push(album); // pushes album object to collection array
+  return album;
+} // end addToCollection
 
-addToCollection( 'Evil Empire', 'Rage Against the Machine', 1996)
-console.log(collection);
+console.log(addToCollection( 'Evil Empire', 'Rage Against the Machine', 1996));
 
 // console.log(collection[0].title);
 // console.log(collection[0].artist);
 // console.log(collection[0].yearPublished);
 // console.log(collection.length);
 
-addToCollection( 'Battle of Los Angeles', 'Rage Against the Machine', 1999)
-console.log(collection);
+console.log(addToCollection( 'Battle of Los Angeles', 'Rage Against the Machine', 1999 ));
+console.log(addToCollection( 'Dreamland', 'Glass Animals', 2020 ));
+console.log(addToCollection( 'How to be a Human Being', 'Glass Animals', 2016 ));
+console.log(addToCollection( 'Run the Jewels 2', 'Run the Jewels', 2014 ));
+console.log(addToCollection( 'Awake', 'Tycho', 2014 ));
 
-addToCollection( 'Dreamland', 'Glass Animals', 2020)
-console.log(collection);
-
-addToCollection( 'How to be a Human Being', 'Glass Animals', 2016)
-console.log(collection);
-
-addToCollection( 'Run the Jewels 2', 'Run the Jewels', 2014)
-console.log(collection);
-
-addToCollection( 'Awake', 'Tycho', 2014)
 console.log(collection);
