dace3072b7c7e43c72c4b274382bac6976d5b7fa
Brant McCarthy <brant.mccarthy@me.com>
2022-01-14 20:08:53 -0600

    finished most of the stretch goals. these are tough!

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -15,16 +15,17 @@ console.log('///// Testing addToCollection function /////');
 let collection = [];
 
 function addToCollection( title, artist, yearPublished ) {
-  let album = { // creates object with keys defined below
+  let album = {     // creates object with keys defined below
     title: title,
     artist: artist,
     yearPublished: yearPublished,
+    tracks: []
   }
-  collection.push(album); // pushes album object to collection array
+  collection.push(album);     // pushes album object to collection array
   return album;
 } // end addToCollection
 
-console.log(addToCollection( 'Evil Empire', 'Rage Against the Machine', 1996));
+console.log(addToCollection( 'Evil Empire', 'Rage Against the Machine', 1996 ));
 console.log(addToCollection( 'Battle of Los Angeles', 'Rage Against the Machine', 1999 ));
 console.log(addToCollection( 'Dreamland', 'Glass Animals', 2020 ));
 console.log(addToCollection( 'How to be a Human Being', 'Glass Animals', 2016 ));
@@ -95,7 +96,7 @@ function search( artist, year ) {
     }
   }
   // if NO matching results
-  if (searchResult.length === 0) { 
+  if (searchResult.length === 0) {
     return `No results found for '${artist}' in year '${year}'`;
   }
   // if matching results
@@ -107,3 +108,87 @@ function search( artist, year ) {
 
 console.log(search('Ray Charles', 1957));
 console.log(search('Glass Animals', 2016));
+
+console.log('///// STRETCH GOAL: adding tracks  /////');
+
+function addTracks( title, name, duration ) {
+  for (i=0; i < collection.length; i++){
+    if (title === collection[i].title){
+      let track = {
+        name: name,
+        duration: duration,
+      }
+      collection[i].tracks.push(track)
+    }
+  }
+}
+
+addTracks('Evil Empire', 'People of the Sun', '2:30')
+addTracks('Evil Empire', 'Bulls on Parade', '3:49')
+addTracks('Evil Empire', 'Vietnow', '4:39')
+addTracks('Evil Empire', 'Revolver', '5:30')
+addTracks('Evil Empire', 'Snakecharmer', '3:56')
+addTracks('Evil Empire', 'Tire Me', '3:00')
+addTracks('Evil Empire', 'Down Rodeo', '5:20')
+addTracks('Evil Empire', 'Without a Face', '3:36')
+addTracks('Evil Empire', 'Wind Below', '5:50')
+addTracks('Evil Empire', 'Roll Right', '4:22')
+addTracks('Evil Empire', 'Year of tha Boomerang', '4:02')
+
+addTracks('Battle of Los Angeles', 'Testify', '3:30')
+addTracks('Battle of Los Angeles', 'Guerilla Radio', '3:26')
+addTracks('Battle of Los Angeles', 'Calm Like a Bomb', '4:58')
+addTracks('Battle of Los Angeles', 'Mic Check', '3:33')
+addTracks('Battle of Los Angeles', 'Sleep Now in the Fire', '3:25')
+addTracks('Battle of Los Angeles', 'Born of a Broken Man', '4:40')
+addTracks('Battle of Los Angeles', 'Born as Ghosts', '3:22')
+addTracks('Battle of Los Angeles', 'Maria', '3:48')
+addTracks('Battle of Los Angeles', 'Voice of the Voiceless', '2:31')
+addTracks('Battle of Los Angeles', 'New Millennium Homes', '3:44')
+addTracks('Battle of Los Angeles', 'Ashes in the Fall', '4:37')
+addTracks('Battle of Los Angeles', 'War Within a Breath', '3:36')
+
+addTracks('Dreamland', 'Dreamland', '3:23')
+addTracks('Dreamland', 'Tangerine', '3:20')
+addTracks('Dreamland', '((Home Movie: 1994))', '0:07')
+addTracks('Dreamland', 'Hot Sugar', '3:54')
+addTracks('Dreamland', '((Home Movie: BTX))', '0:13')
+addTracks('Dreamland', 'Space Ghost Coast to Coast', '3:07')
+addTracks('Dreamland', 'Tokyo Drifting', '3:36')
+addTracks('Dreamland', 'Melon and the Coconut', '2:28')
+addTracks('Dreamland', 'Your Love (Déjà Vu)', '3:54')
+addTracks('Dreamland', 'Waterfalls Coming Out of Your Mouth', '2:41')
+addTracks('Dreamland', 'Its All So Incredibly Loud', '4:19')
+addTracks('Dreamland', '((Home Movie: Rockets))', '1:00')
+addTracks('Dreamland', 'Domestic Bliss', '3:18')
+addTracks('Dreamland', 'Heat Waves', '3:58')
+addTracks('Dreamland', '((Home Movie: Shoes On))', '0:31')
+addTracks('Dreamland', 'Helium', '5:28')
+
+console.log('///// STRETCH GOAL: Adding search functionality for trackName /////');
+
+function searchUpdate ( artist, year, trackName ) {
+  let searchResult = [];
+  for (i=0; i < collection.length; i++) { // loops through collection
+    if ( artist === collection[i].artist && year === collection[i].yearPublished) {  //
+      for (x=0; x < collection[i].tracks.length; x++) {
+        if ( collection[i].tracks[x].name.indexOf(trackName) > -1) {
+          searchResult.push(collection[i])
+        }
+      }
+    }
+  }
+  // if NO matching results
+  if (searchResult.length === 0) {
+    return `No results found for '${trackName}' by ${artist} in year ${year}`;
+  }
+  // if matching results
+  else {
+    console.log(`Matching albums including '${trackName}' by ${artist} in year ${year}`);
+    return searchResult;
+  }
+} // end search
+
+console.log(searchUpdate( 'Glass Animals', 2020, 'Tangerine')); // expect results found
+console.log(searchUpdate( 'Rage Against the Machine', 1999, 'Testify')); // expect results found
+console.log(searchUpdate( 'Rage Against the Machine', 1996, 'Testify')); // expect NO results found
